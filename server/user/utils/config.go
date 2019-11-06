b605b7eaddf4d366fb40bc9d795a0b2ff70db2e0
Toringol <37870973+Toringol@users.noreply.github.com>
2019-11-06 13:00:04 +0300

    Update config.go

--- server/user/utils/config.go
+++ server/user/utils/config.go
@@ -1,10 +1,10 @@
 package utils
 
 const (
-	frontIP        = "http://93.171.139.195:780"
-	backIP         = "http://93.171.139.196:780"
-	listenAddr     = "172.26.112.3:80"
-	dataBaseConfig = "server:12345@tcp(localhost:3306)/users?"
-	pathToImages   = `/root/golang/server_with_db/2019_2_TODO/server/`
+	FrontIP        = "http://93.171.139.195:780"
+	BackIP         = "http://93.171.139.196:780"
+	ListenAddr     = "172.26.112.3:80"
+	DataBaseConfig = "server:12345@tcp(localhost:3306)/users?"
+	PathToImages   = `/root/golang/server_with_db/2019_2_TODO/server/`
 	Secret         = `askhgashjasl;hjaojgh;asjha;shm;`
 )
