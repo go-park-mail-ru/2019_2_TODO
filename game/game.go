6b7de8bcd59915047e747974c19e33239fc066b1
Toringol <noneoftoringol@gmail.com>
2019-11-27 13:43:08 +0300

    Grcp added

--- game/game.go
+++ game/game.go
@@ -1,13 +1,16 @@
 package game
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"net/url"
 
+	"github.com/go-park-mail-ru/2019_2_TODO/tree/devRK/game/core"
+	"github.com/go-park-mail-ru/2019_2_TODO/tree/devRK/server/user/utils"
 	"github.com/go-park-mail-ru/2019_2_TODO/tree/devRK/session"
 
-	"github.com/go-park-mail-ru/2019_2_TODO/tree/devRK/game/core"
+	"google.golang.org/grpc"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
@@ -22,6 +25,20 @@ const (
 var (
 	sessManager session.AuthCheckerClient
 )
+
+type tokenAuth struct {
+	Token string
+}
+
+func (t *tokenAuth) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"session_token": t.Token,
+	}, nil
+}
+
+func (c *tokenAuth) RequireTransportSecurity() bool {
+	return false
+}
 
 type JSONRooms struct {
 	Rooms map[string]int `json:"rooms"`
@@ -57,6 +74,19 @@ func wsHandler(ctx echo.Context) error {
 	params, _ := url.ParseQuery(ctx.Request().URL.RawQuery)
 	if len(params["name"]) > 0 {
 		playerName = params["name"][0]
+	}
+
+	ctxSes := context.Background()
+	nothing := &session.Nothing{}
+	sessionData, err := sessManager.HandleSignInGet(ctxSes, nothing)
+
+	if err != nil {
+		log.Println("Error while grcp session check")
+	}
+
+	emptySession := &session.Session{}
+	if sessionData != emptySession {
+		playerName = sessionData.GetUsername()
 	}
 
 	var roomName string = "newRoom"
@@ -95,6 +125,20 @@ func main() {
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 		AllowCredentials: true,
 	}))
+
+	grcpConn, err := grpc.Dial(
+		utils.FrontIP,
+		grpc.WithPerRPCCredentials(&tokenAuth{"session_token"}),
+		grpc.WithInsecure(),
+	)
+
+	if err != nil {
+		log.Fatalf("cant connect to grpc")
+	}
+
+	defer grcpConn.Close()
+
+	sessManager = session.NewAuthCheckerClient(grcpConn)
 
 	e.GET("/rooms/", func(ctx echo.Context) error {
 		getRooms(ctx)
