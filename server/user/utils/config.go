c3e1d98ae2968a3e9c81a2973fc3c33d50e010a8
Toringol <noneoftoringol@gmail.com>
2019-12-12 14:11:38 +0300

    Https to http

--- server/user/utils/config.go
+++ server/user/utils/config.go
@@ -3,11 +3,11 @@ package utils
 import "github.com/microcosm-cc/bluemonday"
 
 const (
-	FrontIP        = "http://93.171.139.195:743"
+	FrontIP        = "http://93.171.139.195:780"
 	FrontIPChat    = "http://93.171.139.195:781"
-	BackIP         = "http://93.171.139.196:743"
+	BackIP         = "http://93.171.139.196:780"
 	ConsulAddr     = "127.0.0.1:8500"
-	ListenAddr     = "172.26.112.3:443"
+	ListenAddr     = "172.26.112.3:80"
 	DataBaseConfig = "server:12345@tcp(localhost:3306)/users?"
 	PathToImages   = `/root/golang/server_with_db/2019_2_TODO/server/`
 	Secret         = `askhgashjasl;hjaojgh;asjha;shm;`
