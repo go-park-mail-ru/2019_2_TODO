41a0b3e537146929c1cf52d2b54c2c9be08569bc
Toringol <noneoftoringol@gmail.com>
2019-12-12 21:13:56 +0300

    Fixed cookies problems

--- server/user/delivery/http/handlers.go
+++ server/user/delivery/http/handlers.go
@@ -72,10 +72,10 @@ func (h *Handlers) handleSignUp(ctx echo.Context) error {
 		ctx.JSON(http.StatusInternalServerError, "Cookie set error")
 	}
 
-	err = utils.SetToken(ctx)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "Token set error")
-	}
+	// err = utils.SetToken(ctx)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, "Token set error")
+	// }
 
 	log.Println(newUserInput.Username)
 
@@ -110,10 +110,10 @@ func (h *Handlers) handleSignIn(ctx echo.Context) error {
 		ctx.JSON(http.StatusInternalServerError, "Cookie set error")
 	}
 
-	err = utils.SetToken(ctx)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "Token set error")
-	}
+	// err = utils.SetToken(ctx)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, "Token set error")
+	// }
 
 	return nil
 }
