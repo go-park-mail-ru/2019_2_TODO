fd04650365271c718567053fdd872129275ee2e5
Toringol <noneoftoringol@gmail.com>
2019-12-14 13:24:56 +0300

    Add http to https

--- game/game.go
+++ game/game.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	ListenAddr = "172.26.112.3:82"
+	ListenAddr = "172.26.112.3:443"
 	FrontIP    = "http://93.171.139.195:780"
 )
 
@@ -100,5 +100,5 @@ func main() {
 		return nil
 	})
 
-	e.Logger.Fatal(e.Start(ListenAddr))
+	e.Logger.Fatal(e.StartTLS(ListenAddr, "cert.crt", "key.crt"))
 }
